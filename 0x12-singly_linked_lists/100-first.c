e4c91e20d9ecc5295871ce48084aa9260176dc03
SMBABAYO <smbabayo@gmail.com>
2023-10-20 13:11:26 -0700

    first

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+void ComefirstbyCTC(void)__attribute__ ((constructor));
+/**
+* ComefirstbyCTC - Function that runs before main function
+* Return: ..
+*/
+void ComefirstbyCTC(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
