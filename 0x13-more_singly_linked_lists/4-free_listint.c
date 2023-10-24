d876f792141b45f66dca3b0656112c6be7db248d
SMBABAYO <smbabayo@gmail.com>
2023-10-24 12:58:59 -0700

    command

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+
+/**
+* free_listint - a function that frees a linked list
+* @head: pointer to the first node
+* Return: no return
+*/
+void free_listint(listint_t *head)
+{
+	listint_t *temp;
+
+	while ((temp = head) != NULL)
+	{
+		head = head->next;
+		free(temp);
+	}
+}
