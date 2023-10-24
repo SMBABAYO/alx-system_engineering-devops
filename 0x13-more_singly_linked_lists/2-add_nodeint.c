aefc9cb9b74e30cf3ac64b9906c933954ffedf21
SMBABAYO <smbabayo@gmail.com>
2023-10-24 15:16:08 -0700

    add

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -2,9 +2,7 @@
 
 /**
 * add_nodeint - add node to the beginning of a list
-*
 * @head: pointer to the head pointer
-*
 * @n: new int to be created
 * Return: head
 */
@@ -16,7 +14,8 @@ listint_t *add_nodeint(listint_t **head, const int n)
 		return (NULL);
 
 	new_node = malloc(sizeof(listint_t));
-
+	if (new_node == NULL)
+		return (NULL);
 		if (*head == NULL)
 			new_node->next = NULL;
 		else
