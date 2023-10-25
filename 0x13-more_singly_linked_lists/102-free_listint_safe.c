80fdb0617f8355195c74e12224eb07c4a5cc4910
SMBABAYO <smbabayo@gmail.com>
2023-10-25 13:50:01 -0700

    done

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,11 +1,11 @@
 #include "lists.h"
 
 /**
-* free_listp - free a linked list
+* free_listp2 - free a linked list
 * @head: head of list
 * Return: no return
 */
-void free_listp(listp_t **head)
+void free_listp2(listp_t **head)
 {
 	listp_t *temp;
 	listp_t *curr;
@@ -17,50 +17,52 @@ void free_listp(listp_t **head)
 		{
 			curr = curr->next;
 			free(temp);
-
 		}
+		
 		*head = NULL;
 	}
 }
 
 /**
-* print_listint_safe - prints a linked list
+* free_listint_safe - prints a linked list
 * @head: pointer to list
 * Return: number of nodes
 */
-size_t print_listint_safe(const listint_t *head)
-{
-	size_t nnodes = 0;
+size_t free_listint_safe(listint_t **h)
+size_t nnodes = 0;
 	listp_t *hptr, *new, *add;
+	listint_t *curr;
 
 	hptr = NULL;
-	while (head != NULL)
-	{
+	while (*h != NULL)
+        {
 		new = malloc(sizeof(listp_t));
-
 		if (new == NULL)
 			exit(98);
-
-		new->p = (void *)head;
+		
+		new->p = (void *)*h;
 		new->next = hptr;
 		hptr = new;
-
+		
 		add = hptr;
-
+		
 		while (add->next != NULL)
 		{
 			add = add->next;
-			if (head == add->p)
+			if (*h == add->p)
 			{
-				printf("-> [%p] %d\n", (void *)head, head->n);
-				free_listp(&hptr);
+				*h = NULL;
+				free_listp2(&hptr);
 				return (nnodes);
 			}
 		}
-		printf("[%p] %d\n", (void *)head, head->n);
-		head = head->next;
-		nnodes++;
+		curr = *h;
+		*h = head->next
+		free(curr);
+		nnodes++
 	}
-	free_listp(&hptr);
+
+	*h = NULL;
+	free_listp2(&hptr);
 	return (nnodes);
 }
